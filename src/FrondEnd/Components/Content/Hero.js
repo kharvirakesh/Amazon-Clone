b1c4fd26e1a7a97e95e0af644a1129ac7e8bdcd4
kharvirakesh <kharvirakesh3@gmail.com>
2021-05-20 13:11:12 +0530

    Home Page contents added

--- src/FrondEnd/Components/Content/Hero.js
+++ src/FrondEnd/Components/Content/Hero.js
@@ -1,40 +1,40 @@
 import React from "react";
 import { Carousel } from "antd";
-
-function Hero(props) {
+import "../Content/Hero.css";
+function Hero() {
   const contentStyle = {
-    height: "160px",
-    color: "#fff",
+    height: "100vh",
+    color: "red",
     lineHeight: "160px",
     textAlign: "center",
-    background: "#364d79",
+    // background: "#364d79",
   };
 
   const items = [
     {
       key: 1,
       title: "title-1",
-      Content: "content-1",
+      Content: "necessitatibus officiis repudiandae est deserunt delectus dolorem iure porro distinctio fuga, nostrum doloremque",
     },
     {
       key: 2,
       title: "title-2",
-      Content: "content-2",
+      Content: "the epolepels reeosifeet",
     },
     {
       key: 3,
       title: "title-3",
-      Content: "content-3",
+      Content: "dgeebejkheeem, klthe lxlxlxl the sismlts sksks ebejkhe",
     },
   ];
   return (
-    <div>
+    <div className="heroBlock">
       <Carousel autoplay>
         {items.map((item) => {
           return (
             <div>
-              <h3 style={contentStyle}>{item.title}</h3>
-              <h3>{item.Content}</h3>
+              <h3 style={contentStyle}>{item.Content}</h3>
+              {/* <p>{item.Content}</p> */}
             </div>
           );
         })}
--- src/FrondEnd/Views/Home.js
+++ src/FrondEnd/Views/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import About from '../Components/Content/About'
+import Feature from '../Components/Content/Feature'
 import Hero from '../Components/Content/Hero'
+import Works from '../Components/Content/Works'
 
 function AppHome() {
     return (
         <div>
             <Hero/>
+            <About/>
+            <Feature/>
+            <Works/>
         </div>
     )
 }
